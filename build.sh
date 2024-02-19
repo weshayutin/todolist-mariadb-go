fea0f334723e76acef3361b866cdf236d9195532
Wesley Hayutin <weshayutin@gmail.com>
2024-02-19 10:58:35 -0700

    update build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,7 @@ GOOS=linux GOARCH=amd64 go build -o BUILD/amd64/todolist-linux-amd64 .
 cp -rv resources BUILD/amd64/
 cp -v favicon.ico BUILD/amd64/
 cp -v index.html BUILD/amd64/
-zip -r BUILD/amd64/todolist-linux-amd64.zip BUILD/amd64/*
+pushd BUILD/amd64/
+zip -r todolist-linux-amd64.zip *
+popd
 echo "done"
